40b8170f4c38d1deb93eb8274e715b229b58f8fb
matsumatsu233 <cuizhen199209@gmail.com>
2017-11-13 22:18:51 +0900

    Update strip_zero_from_filename.py

--- strip_zero_from_filename.py
+++ strip_zero_from_filename.py
@@ -1,7 +1,9 @@
 import os
 
-files = os.listdir("./")
+FRAMES_JPG_DIR = "./frames_jpg/";
+files = os.listdir(FRAMES_JPG_DIR)
 
 for key in range(0, len(files)):
   if files[key][0] == "0":
-    os.rename(files[key], files[key].strip("0"))
+    print("rename " + files[key] + " to " + files[key].strip("0"))
+    os.rename(FRAMES_JPG_DIR + files[key], FRAMES_JPG_DIR + files[key].strip("0"))
