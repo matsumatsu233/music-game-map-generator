842deaaeb01f0b1b004315a2f71f3cf67c88745c
matsumatsu233 <cuizhen199209@gmail.com>
2018-03-25 21:34:24 +0900

    Output json result by notes

--- map-generator/src/step1_templateMapping.py
+++ map-generator/src/step1_templateMapping.py
@@ -25,6 +25,23 @@ def getVerticalPosition(resultPositions):
     result.append(bufferArray[int((len(bufferArray)-1)/2)])
   return result
 
+def getRoughInterval(totalResult, firstNotEmpty):
+  result = {}
+  for index in range(firstNotEmpty, firstNotEmpty + 20):
+    for X in range(len(totalResult[index+1])):
+      for Y in range(len(totalResult[index])):
+        if totalResult[index+1][X] > totalResult[index][Y]:
+          sub = totalResult[index+1][X] - totalResult[index][Y]
+          if sub in result:
+            result[sub] +=1
+          else:
+            result[sub] = 1
+  return max(result, key=result.get)
+
+def printNotes(notes):
+  print("notes:")
+  for noteIndex in range(0, len(notes)):
+    print(str(noteIndex) + ": " + str(notes[noteIndex]))
 
 # Get position from images
 imgPathTemplate = Template("../input/frames_jpg/${index}.jpg")
@@ -34,11 +51,11 @@ total = 4004
 step = 1
 
 totalResult = []
-jsonResult = []
+#jsonResult = []
 firstNotEmpty = 0
 
 with open(outputPath, 'w') as resultFile:
-  for index in range(1,total):
+  for index in range(1, total):
     print("processing " + str(index) + "/" + str(total))
 
     imgPath = imgPathTemplate.substitute(index=index*step)
@@ -51,9 +68,10 @@ with open(outputPath, 'w') as resultFile:
     resultPositions = zip(*np.where(templateMappingresult >= threshold)[::-1])
 
     result = getVerticalPosition(resultPositions)
-    jsonResult.append({
-      "snare": result
-    })
+    totalResult.append(result)
+    #jsonResult.append({
+    #  "snare": result
+    #})
     print("result: " + str(result))
     #resultFile.write(" index: " + str(index * step) + " " + str(final) + "\n")
 
@@ -61,5 +79,40 @@ with open(outputPath, 'w') as resultFile:
     if (firstNotEmpty == 0) and result:
       firstNotEmpty = index
   
-  print(jsonResult)
-  json.dump(jsonResult, resultFile, indent=2)
+  #print(jsonResult)
+  #json.dump(jsonResult, resultFile, indent=2)
+
+  roughInterval = getRoughInterval(totalResult, firstNotEmpty)
+  print("roughInterval " + str(roughInterval))
+
+  notes = []
+  current = 0
+  tolerance = 2
+  firstPositionLimitation = 50
+
+  for index in range(0, len(totalResult)):
+    for notePosition in totalResult[index]:
+      currentNote = {
+        "index": index,
+        "position": notePosition
+      }
+      if len(notes) == 0: # To distinguish the first time
+        notes.append([currentNote])
+      else:
+        isOldNote = False
+        for noteIndex in range(0, len(notes)):
+          if (abs(notePosition - notes[noteIndex][-1]["position"] - roughInterval) < tolerance
+            and index == notes[noteIndex][-1]["index"] + 1):
+            #print("old note!")
+            notes[noteIndex].append(currentNote)
+            isOldNote = True
+            break
+        if not isOldNote:
+          # Too large notePosition means this note has already been recognized before
+          if notePosition < firstPositionLimitation:
+            #print("new note!")
+            notes.append([currentNote])
+  json.dump(notes, resultFile, indent=2)
+  printNotes(notes)
+
+
